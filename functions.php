a669855958ce29fc74f87ac7cc9ce488716b4736
freitasdev <frtecnologiadigital@gmail.com>
2024-09-11 14:09:42 -0300

    CRT-ES

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+//INCLUDES
+
+//HOOKS
+
+//carregar css e js
+function fr_scripts(){
+    wp_enqueue_style('main',urlFile('espiritosantostyle.css'));
+    wp_enqueue_style('font-awesome',urlFile('fontawesome/css/all.css'));
+    wp_enqueue_style('bootstrap','https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+    wp_register_script('bootstrapjs','https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js',array(), '5.3.3', true);
+}
+
+function url($anexo){
+    echo get_template_directory_uri()."/".$anexo;
+}
+
+function urlFile($anexo){
+    return get_template_directory_uri()."/".$anexo;
+}
+
+//
+function fr_theme_support(){
+    add_theme_support('title-tag');
+    add_theme_support('custom_logo');
+    add_theme_support('post_thumbnails');
+    register_nav_menu('primary',__('Menu Principal','CRT-ES'));
+}
+
+add_action('wp_enqueue_scripts','fr_scripts');
+//adicionar recursos do wp
+add_action('after_setup','fr_theme_support');
---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CRT-ES (Conselho Regional de Técnicos Industriais do Espirito Santo)</title>
+    <?php wp_head();?>
+</head>
+<body>
+<header>
+<!--FIRST HEADER-->
+<nav class="navbar navbar-expand-lg crtlogobg">
+  <div class="container-fluid">
+    <a class="text-wrap text-white" href="#" style="text-decoration:none; color:black;"><i class="fa-solid fa-location-dot"></i> Av. Nossa Srª dos Navegantes, Edifício Palácio do Café, 675, Sala 701, Enseada do Suá, Vitória - ES, CEP 29.050-912</a>
+    <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fa-brands fa-facebook-f"></i> Facebook</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fa-brands fa-youtube"></i> Youtube</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fa-brands fa-linkedin"></i> Linkedin</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#"><i class="fa-brands fa-instagram"></i> Instagram</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!--SECOND HEADER-->
+<div class="row">
+  <div class="col-sm-6 d-flex justify-content-center p-3">
+    <img src="<?php url('logoheader.png')?>"></img>
+  </div>
+  <div class="col-sm-6 p-3">
+    <ul>
+      <h5 class="crtcolor"><i class="fa-solid fa-phone" style="font-size:15px;"></i> Central de Atendimento</h5>
+      <li>(27) 3100-2019</li>
+      <li>(27) 9 9933-7517 (Somente Ligação)</li>
+      <li>(27) 9 9689-9239 (Somente WhatsApp)</li>
+      <li>atendimento@crtes.gov.br</li>
+      <li><strong>Cobrança/Dívida Ativa</strong> (27) 99867-5197 | regularize@crtes.gov.br</li>
+      <li><strong>Funcionamento:</strong><br>Segunda a sexta-feira de 8h às 17h</li>
+    </ul>
+  </div>
+</div>
+<!--THREE HEADER-->
+<nav class="navbar navbar-expand-lg crtbg ">
+  <div class="container-fluid">
+    <a class="btn btn-outline-secondary text-white" href="#">Espaço do Técnico</a>
+    <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent2" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent2">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <!--INÍCIO-->
+        <li class="nav-item">
+          <a class="nav-link text-white" href="#">Início</a>
+        </li>
+        <!--INSTITUICIONAL-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Instituicional
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Missão. visão e valores</a></li>
+            <li><a class="dropdown-item" href="#">Hostória</a></li>
+            <li><a class="dropdown-item" href="#">Modalidades Técnicas</a></li>
+            <li><a class="dropdown-item" href="#">Eleições 2022</a></li>
+          </ul>
+        </li>
+        <!--LEGISLAÇÃO FEDERAL-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Legislação Federal
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Legislação Federal</a></li>
+            <li><a class="dropdown-item" href="#">Outros Documentos</a></li>
+            <li><a class="dropdown-item" href="#">Resolução CFT</a></li>
+            <li><a class="dropdown-item" href="#">Impugnações</a></li>
+          </ul>
+        </li>
+        <!--TRANSPARÊNCIA-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Transparência
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Portal da Transparência</a></li>
+            <li><a class="dropdown-item" href="#">Ouvidoria</a></li>
+          </ul>
+        </li>
+        <!--SERVIÇOS ONLINE-->
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle text-white" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Serviços Online
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Solicitar Registral Profissional</a></li>
+            <li><a class="dropdown-item" href="#">Solicitar Registro de Empresa</a></li>
+            <li><a class="dropdown-item" href="#">Pesquisar Profissional/Empresa</a></li>
+            <li><a class="dropdown-item" href="#">Validar Certidões</a></li>
+            <li><a class="dropdown-item" href="#">Validar TRT</a></li>
+            <li><a class="dropdown-item" href="#">CAT</a></li>
+            <li><a class="dropdown-item" href="#">Denuncia</a></li>
+            <li><a class="dropdown-item" href="#">Pesquisar Denuncia</a></li>
+          </ul>
+        </li>
+        <!--PERGUNTAS FREQUENTES-->
+        <!--PAINEL DE ANUIDADE-->
+        <!--FIM DO MENU-->
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Pesquisar" aria-label="Search">
+        <button class="btn btn-outline-secondary" type="submit"><i class="fas fa-search"></i></button>
+      </form>
+    </div>
+  </div>
+</nav>
+<!--FIM DOS CABECALHOS-->
+</header>
+<main>
+
+</main>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+</body>
+</html>
